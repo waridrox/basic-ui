4c231132ae5b47e2c60c11e7676eeb8bbc151c01
Shubham Karande <shubhamkarande13@gmail.com>
2021-06-16 01:13:22 +0530

    add Dockerfile and API support

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@
 
         </md-card-content>
 
-
       </md-card>
 
       <md-card>
@@ -37,37 +36,57 @@
 </template>
 
 <script>
+import './main.js'
+const axios = require('axios')
 export default {
   name: 'App',
-  data: function() {
+  data: function () {
     return {
-      title: "Displays the current execution" 
+      title: 'Displays the current execution'
     }
   },
   methods: {
-    apiRoute_spec: function() {
-      this.title = "SPEC route invoked";
-      return this.title;
+    apiRoute_spec: function () {
+      // fetch('http://127.0.0.1:8080/spec')
+      // .then(response => response.json())
+      // .then(json => console.log(json))
+      axios.get('http://127.0.0.1:8080/spec')
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+      this.title = 'SPEC route invoked'
+      return this.title
     },
-    apiRoute_help: function() {
-      this.title = "HELP route invoked";
-      return this.title;
-    }, 
-    apiRoute_version: function() {
-      this.title = "VERSION route invoked";
-      return this.title;
-    }, 
-    apiRoute_point: function() {
-      this.title = "POINT route invoked";
-      return this.title;
-    }, 
-    apiRoute_rothc: function() {
-      this.title = "ROTHC route invoked";
-      return this.title;
-    }, 
-    cancel_operation: function() {
-      this.title = "Operation was cancelled";
-      return this.title;
+    apiRoute_help: function () {
+      axios.get('http://127.0.0.1:8080/help/all')
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+      this.title = 'HELP route invoked'
+      return this.title
+    },
+    apiRoute_version: function () {
+      axios.get('http://127.0.0.1:8080/version')
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+      this.title = 'VERSION route invoked'
+      return this.title
+    },
+    apiRoute_point: function () {
+      axios.post('http://127.0.0.1:8080/point')
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+      this.title = 'POINT route invoked'
+      return this.title
+    },
+    apiRoute_rothc: function () {
+      axios.post('http://127.0.0.1:8080/rothc')
+        .then(response => console.log(response))
+        .catch(error => console.log(error))
+      this.title = 'ROTHC route invoked'
+      return this.title
+    },
+    cancel_operation: function () {
+      this.title = 'Operation was cancelled'
+      return this.title
     }
   }
 }
